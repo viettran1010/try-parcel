ac9f69c24b1deceabb390078d353b73bab0fb20e
viettran1010 <viettran10101988@gmail.com>
2021-07-25 17:34:30 +0700

    remove node_modules

--- dist/try-parcel.f69400ca.js
+++ dist/try-parcel.f69400ca.js
@@ -29760,7 +29760,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61420" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60863" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
